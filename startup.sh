f7b39952c886a5f736629357472cf75e78bd8d9d
Jesse Patching <taeram@users.noreply.github.com>
2021-05-18 21:00:26 -0600

    Ensure the xdebug folder is writable

--- startup.sh
+++ startup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # Setup xdebug.
-XDEBUG_REMOTE_HOST=$( /sbin/ip route | awk '/default/ { print $3 }' )
-
 tee /usr/local/etc/php/conf.d/99-radpenguin.ini << EOF
-xdebug.client_host=${XDEBUG_REMOTE_HOST}
 xdebug.mode=${XDEBUG_MODE:-off}
 EOF
 
+# Ensure the xdebug folder is writable.
+if [[ "$XDEBUG_MODE" != "off" ]]; then
+  mkdir -p /var/www/html/.xdebug/
+  chown -R www-data:www-data /var/www/html/.xdebug/
+fi
