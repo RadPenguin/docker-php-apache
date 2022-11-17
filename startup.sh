c2b8faa8f6789390556af036657b77ce29b6459e
Jesse Patching <jesse@patching.ca>
2022-11-16 22:46:29 -0700

    No move, just copy.

--- startup.sh
+++ startup.sh
@@ -4,7 +4,7 @@
 XDEBUG_MODE=${XDEBUG_MODE:-off}
 if [[ "$XDEBUG_MODE" != "off" ]]; then
   # Enable the development mode php ini.
-  mv /usr/local/etc/php/php.ini-development /usr/local/etc/php/php.ini
+  cp /usr/local/etc/php/php.ini-development /usr/local/etc/php/php.ini
   tee /usr/local/etc/php/conf.d/99-radpenguin.ini << EOF
 extension_xdebug = 1
 opcache.revalidate_freq = 0
